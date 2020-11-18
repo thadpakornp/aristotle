0810d182f7ce3222e3469001400af53a5cb74bd5
thadpakornp <thadpakorn.p@outlook.com>
2020-11-18 11:51:43 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,7 +21,8 @@
 
     <!-- Vendor CSS-->
     <link href="{{ asset('vendor/animsition/animsition.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet"
+          media="all">
     <link href="{{ asset('vendor/wow/animate.css') }}" rel="stylesheet" media="all">
     <link href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
     <link href="{{ asset('vendor/slick/slick.css') }}" rel="stylesheet" media="all">
@@ -31,9 +32,11 @@
     <!-- Main CSS-->
     <link href="{{ asset('css/theme.css') }}" rel="stylesheet" media="all">
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/modal-loading.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/modal-loading-animate.css') }}" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.10.1/sweetalert2.min.css" integrity="sha512-zEmgzrofH7rifnTAgSqWXGWF8rux/+gbtEQ1OJYYW57J1eEQDjppSv7oByOdvSJfo0H39LxmCyQTLOYFOa8wig==" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/modal-loading.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/modal-loading-animate.css') }}"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.10.1/sweetalert2.min.css"
+          integrity="sha512-zEmgzrofH7rifnTAgSqWXGWF8rux/+gbtEQ1OJYYW57J1eEQDjppSv7oByOdvSJfo0H39LxmCyQTLOYFOa8wig=="
+          crossorigin="anonymous"/>
     @yield('style')
 </head>
 
@@ -45,7 +48,7 @@
             <div class="container-fluid">
                 <div class="header-mobile-inner">
                     <a class="logo" href="#">
-                        <img src="{{ asset('images/icon/logo.png') }}" alt="" />
+                        <img src="{{ asset('images/icon/logo.png') }}" alt=""/>
                     </a>
                     <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
@@ -58,10 +61,6 @@
         <nav class="navbar-mobile">
             <div class="container-fluid">
                 <ul class="navbar-mobile__list list-unstyled">
-                    <li>
-                        <a href="">
-                            <i class="far fa-check-square"></i>Create channel</a>
-                    </li>
                     @role('admin')
                     <li class="has-sub">
                         <a class="js-arrow" href="#">
@@ -86,16 +85,12 @@
     <aside class="menu-sidebar d-none d-lg-block">
         <div class="logo">
             <a href="#">
-                <img src="{{ asset('images/icon/logo.png') }}" alt="Cool Admin" />
+                <img src="{{ asset('images/icon/logo.png') }}" alt="Cool Admin"/>
             </a>
         </div>
         <div class="menu-sidebar__content js-scrollbar1">
             <nav class="navbar-sidebar">
                 <ul class="list-unstyled navbar__list">
-                    <li>
-                        <a href="">
-                            <i class="far fa-check-square"></i>Create channel</a>
-                    </li>
                     @role('admin')
                     <li class="has-sub">
                         <a class="js-arrow" href="#">
@@ -127,59 +122,64 @@
                         </div>
                         <div class="header-button">
                             <div class="noti-wrap">
-{{--                                <div class="noti__item js-item-menu">--}}
-{{--                                    <i class="zmdi zmdi-notifications"></i>--}}
-{{--                                    <span class="quantity">3</span>--}}
-{{--                                    <div class="notifi-dropdown js-dropdown">--}}
-{{--                                        <div class="notifi__title">--}}
-{{--                                            <p>You have 3 Notifications</p>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="notifi__item">--}}
-{{--                                            <div class="bg-c1 img-cir img-40">--}}
-{{--                                                <i class="zmdi zmdi-email-open"></i>--}}
-{{--                                            </div>--}}
-{{--                                            <div class="content">--}}
-{{--                                                <p>You got a email notification</p>--}}
-{{--                                                <span class="date">April 12, 2018 06:50</span>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="notifi__item">--}}
-{{--                                            <div class="bg-c2 img-cir img-40">--}}
-{{--                                                <i class="zmdi zmdi-account-box"></i>--}}
-{{--                                            </div>--}}
-{{--                                            <div class="content">--}}
-{{--                                                <p>Your account has been blocked</p>--}}
-{{--                                                <span class="date">April 12, 2018 06:50</span>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="notifi__item">--}}
-{{--                                            <div class="bg-c3 img-cir img-40">--}}
-{{--                                                <i class="zmdi zmdi-file-text"></i>--}}
-{{--                                            </div>--}}
-{{--                                            <div class="content">--}}
-{{--                                                <p>You got a new file</p>--}}
-{{--                                                <span class="date">April 12, 2018 06:50</span>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                        <div class="notifi__footer">--}}
-{{--                                            <a href="#">All notifications</a>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
+                                {{--                                <div class="noti__item js-item-menu">--}}
+                                {{--                                    <i class="zmdi zmdi-notifications"></i>--}}
+                                {{--                                    <span class="quantity">3</span>--}}
+                                {{--                                    <div class="notifi-dropdown js-dropdown">--}}
+                                {{--                                        <div class="notifi__title">--}}
+                                {{--                                            <p>You have 3 Notifications</p>--}}
+                                {{--                                        </div>--}}
+                                {{--                                        <div class="notifi__item">--}}
+                                {{--                                            <div class="bg-c1 img-cir img-40">--}}
+                                {{--                                                <i class="zmdi zmdi-email-open"></i>--}}
+                                {{--                                            </div>--}}
+                                {{--                                            <div class="content">--}}
+                                {{--                                                <p>You got a email notification</p>--}}
+                                {{--                                                <span class="date">April 12, 2018 06:50</span>--}}
+                                {{--                                            </div>--}}
+                                {{--                                        </div>--}}
+                                {{--                                        <div class="notifi__item">--}}
+                                {{--                                            <div class="bg-c2 img-cir img-40">--}}
+                                {{--                                                <i class="zmdi zmdi-account-box"></i>--}}
+                                {{--                                            </div>--}}
+                                {{--                                            <div class="content">--}}
+                                {{--                                                <p>Your account has been blocked</p>--}}
+                                {{--                                                <span class="date">April 12, 2018 06:50</span>--}}
+                                {{--                                            </div>--}}
+                                {{--                                        </div>--}}
+                                {{--                                        <div class="notifi__item">--}}
+                                {{--                                            <div class="bg-c3 img-cir img-40">--}}
+                                {{--                                                <i class="zmdi zmdi-file-text"></i>--}}
+                                {{--                                            </div>--}}
+                                {{--                                            <div class="content">--}}
+                                {{--                                                <p>You got a new file</p>--}}
+                                {{--                                                <span class="date">April 12, 2018 06:50</span>--}}
+                                {{--                                            </div>--}}
+                                {{--                                        </div>--}}
+                                {{--                                        <div class="notifi__footer">--}}
+                                {{--                                            <a href="#">All notifications</a>--}}
+                                {{--                                        </div>--}}
+                                {{--                                    </div>--}}
+                                {{--                                </div>--}}
                             </div>
                             <div class="account-wrap">
                                 <div class="account-item clearfix js-item-menu">
                                     <div class="image">
-                                        <img src="@if(auth()->user()->profile == null) {{ asset('images/icon/avatar-01.jpg') }} @else {{ asset(auth()->user()->profile) }} @endif" alt="{{ auth()->user()->name }} {{ auth()->user()->surname }}" />
+                                        <img
+                                            src="@if(auth()->user()->profile == null) {{ asset('images/icon/avatar-01.jpg') }} @else {{ asset(auth()->user()->profile) }} @endif"
+                                            alt="{{ auth()->user()->name }} {{ auth()->user()->surname }}"/>
                                     </div>
                                     <div class="content">
-                                        <a class="js-acc-btn" href="#">{{ auth()->user()->name }} {{ auth()->user()->surname }}</a>
+                                        <a class="js-acc-btn"
+                                           href="#">{{ auth()->user()->name }} {{ auth()->user()->surname }}</a>
                                     </div>
                                     <div class="account-dropdown js-dropdown">
                                         <div class="info clearfix">
                                             <div class="image">
                                                 <a href="#">
-                                                    <img src="@if(auth()->user()->profile == null) {{ asset('images/icon/avatar-01.jpg') }} @else {{ asset(auth()->user()->profile) }} @endif" alt="{{ auth()->user()->name }} {{ auth()->user()->surname }}" />
+                                                    <img
+                                                        src="@if(auth()->user()->profile == null) {{ asset('images/icon/avatar-01.jpg') }} @else {{ asset(auth()->user()->profile) }} @endif"
+                                                        alt="{{ auth()->user()->name }} {{ auth()->user()->surname }}"/>
                                                 </a>
                                             </div>
                                             <div class="content">
@@ -190,6 +190,10 @@
                                             </div>
                                         </div>
                                         <div class="account-dropdown__body">
+                                            <div class="account-dropdown__item">
+                                                <a href="#" class="text-danger">
+                                                    <i class="zmdi zmdi-edit text-danger"></i>Create Channel</a>
+                                            </div>
                                             <div class="account-dropdown__item">
                                                 <a href="#">
                                                     <i class="zmdi zmdi-account"></i>Account</a>
@@ -210,8 +214,8 @@
         <!-- HEADER DESKTOP-->
 
         <!-- MAIN CONTENT-->
-        @yield('content')
-        <!-- END MAIN CONTENT-->
+    @yield('content')
+    <!-- END MAIN CONTENT-->
         <!-- END PAGE CONTAINER-->
     </div>
 
@@ -242,12 +246,14 @@
 
 <!-- Main JS-->
 <script src="{{ asset('js/main.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.10.1/sweetalert2.min.js" integrity="sha512-geFV99KIlNElg1jwzHE6caxE2tLBEw5l+e2cTRPJz273bbiQqpEuqvQzGAmJTdMfUJjoSDXkaUInwjiIz1HfqA==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/10.10.1/sweetalert2.min.js"
+        integrity="sha512-geFV99KIlNElg1jwzHE6caxE2tLBEw5l+e2cTRPJz273bbiQqpEuqvQzGAmJTdMfUJjoSDXkaUInwjiIz1HfqA=="
+        crossorigin="anonymous"></script>
 @yield('script')
 @yield('script_ready')
 
 <script type="text/javascript">
-    $(function (){
+    $(function () {
         // document.addEventListener('contextmenu', function(e) {
         //     e.preventDefault();
         // });
